6d6778c755e3aba7fcea9534d6b0065d0d9aa5e5
ImSurajx <surajxcode@gmail.com>
2026-04-13 19:37:04 +0530

    feat(logic): implement `zoom-in` or `zoom-out`
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- js/script.js
+++ js/script.js
@@ -3,16 +3,42 @@ let selectImg = document.querySelector('#choose-image');
 let imgContainer = document.querySelector('.img-container');
 let imageInputLable = document.querySelector('#img-label');
 
+// varible for zoom-in or zoom-out effect
+let scale = 1;
+let zoomIn = document.querySelector('.ri-zoom-in-line');
+let zoomOut = document.querySelector('.ri-zoom-out-line');
+let zoom = document.querySelector('.zoom');
+
+// track image present of not
+let isImage = false;
+
 // create image & replace it with our container.
 selectImg.addEventListener("change", (e) => {
     let img = document.createElement('img');
+    img.id = 'cont-img';
     const file = e.target.files[0];
     if (!file) {
         return
     }
-    else{
+    else {
         img.src = URL.createObjectURL(file);
         imageInputLable.hidden = true;
         imgContainer.appendChild(img);
+        isImage = true;
     }
+})
+
+zoomIn.addEventListener("click", (e) => {
+    console.log(e);
+    if (scale =< 2) scale += 0.05;
+    else scale = 1;
+    document.querySelector('#cont-img').style.scale = `${scale}`;
+    zoom.textContent = `${Math.floor(scale * 100)}%`;
+})
+zoomOut.addEventListener("click", (e) => {
+    console.log(e);
+    if (scale >= 0) scale -= 0.05;
+    else scale = 1;
+    document.querySelector('#cont-img').style.scale = `${scale}`;
+    zoom.textContent = `${Math.floor(scale * 100)}%`;
 })
