3a4a84dad2e34c2c2a7d6f2f856182d537f4596b
ImSurajx <surajxcode@gmail.com>
2026-04-13 19:04:00 +0530

    feat(logic): implement input & render image
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("connected");
+// all the requried field to get image into img container
+let selectImg = document.querySelector('#choose-image');
+let imgContainer = document.querySelector('.img-container');
+let imageInputLable = document.querySelector('#img-label');
+
+// create image & replace it with our container.
+selectImg.addEventListener("change", (e) => {
+    let img = document.createElement('img');
+    const file = e.target.files[0];
+    if (!file) {
+        return
+    }
+    else{
+        img.src = URL.createObjectURL(file);
+        imageInputLable.hidden = true;
+        imgContainer.appendChild(img);
+    }
+})
