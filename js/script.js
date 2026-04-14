998952fc9e2fd8fd77a0c7f9fad273376e4c6413
ImSurajx <surajxcode@gmail.com>
2026-04-14 13:12:50 +0530

    feat(logic): implement anti-clock rotation for images
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- js/script.js
+++ js/script.js
@@ -14,6 +14,15 @@ let zoom = document.querySelector('.zoom');
 let minZoom = 0.1;
 let maxZoom = 2;
 
+// varibles for rotation
+let antiClock = document.querySelector('.ri-anticlockwise-2-line');
+let rotation = 0;
+
+// this function update image according to user tool
+function updateImage() {
+    imgElement.style.transform = `rotate(${rotation}deg) scale(${scale})`;
+}
+
 // create image & replace it with our container.
 selectImg.addEventListener("change", (e) => {
     let img = document.createElement('img');
@@ -31,15 +40,26 @@ selectImg.addEventListener("change", (e) => {
     }
 })
 
+// when user click zoom-in button this function will run
 zoomIn.addEventListener("click", (e) => {
     if (!imgElement) return;
     if (scale < 2) scale += 0.05;
-    imgElement.style.transform = `scale(${scale})`;
+    updateImage();
     zoom.textContent = `${Math.floor(scale * 100)}%`;
 })
+
+// when user click zoom-out button this function will run
 zoomOut.addEventListener("click", (e) => {
     if (!imgElement) return;
-    if (scale > 0.5) scale -= 0.05;
-    imgElement.style.transform = `scale(${scale})`;
+    if (scale > 0.1) scale -= 0.05;
+    updateImage();
     zoom.textContent = `${Math.floor(scale * 100)}%`;
+})
+
+// when user click rotateIcon this function will run
+antiClock.addEventListener("click", (e) => {
+    if (!imgElement) return;
+    if(rotation >= 360) rotation = 0;
+    else rotation -= 90;
+    updateImage();
 })
